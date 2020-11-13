626475acdad2f9b57f9f93f5a0cc1938eab7af99
reubot <reuben_p@yahoo.com>
2020-11-13 22:42:43 +1000

    Update make_thumbs.py
    get date times

--- make_thumbs.py
+++ make_thumbs.py
@@ -1,7 +1,13 @@
 #!/bin/python
 import argparse
 import os
+import re
 from pathlib import Path
+import subprocess
+from subprocess import PIPE, run,call
+import piexif
+from datetime import datetime, date, time, timezone, timedelta
+
 ## A POSIX variable
 #OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
@@ -12,6 +18,8 @@ from pathlib import Path
 #str starttime=0
 #endtime=0
 #average=0
+
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', metavar='inputfile', type=str, action='store',
@@ -29,7 +37,9 @@ args = parser.parse_args()
 inputfile = args.f
 endtime = args.e
 interval_secs = args.i
+starttime = args.s
 print(args)
+
 
 #show_help ()
 #{
@@ -73,10 +83,29 @@ print(args)
    #exit 1
 #fi
 
+dto = subprocess.run(['exiftool',inputfile,"-DateTimeOriginal"],capture_output=True)
+print(dto)
+dtom=dto.stdout.decode("utf-8").replace(':','').replace('\n','')
+dtom=re.sub("Date/Time Original\s+","",dtom)
+filedate=datetime.strptime(dtom,'%Y%m%d %H%M%S%z')
+print(dtom, filedate)
 if endtime == 0:
-   ## When used to calculate average interverals or end time is not defined...
-    endtime=os.system("ffprobe -i "+inputfile+' -show_entries format=duration -v quiet -of csv="p=0" '+"| sed 's/\..*//'")
-    print(endtime)
+#https://stackoverflow.com/a/30980523/5840230
+
+    duration = subprocess.check_output(['ffprobe', '-i', inputfile, '-show_entries', 'format=duration', '-v', 'quiet', '-of', 'csv=%s' % ("p=0")])
+    print(float(duration.decode("utf-8")))	
+   ### When used to calculate average interverals or end time is not defined...
+    #et=run(["ffprobe",
+	    #"-v", "quiet",
+	    ##" -show_entries", 
+	    #"format=duration", inputfile,
+	    ##'-of csv="p=0" '
+	    ##"| sed 's/\..*//'"
+	    #],capture_output=True)
+    #print(et)
+    #endtime=et.stdout
+    endtime=float(duration.decode("utf-8"))
+    print("endtime\t",endtime)
 #fi
 
 #if [ $average -gt 0 ]; then
@@ -95,34 +124,48 @@ if endtime == 0:
 #filename=`echo $inputfile | sed 's/.*\///'`
 
 filename=Path(inputfile).stem
-print(filename)
+print("stem\t",filename)
 ## Drop off the filename extesion...
 #filename="${filename/.mp4/}"
-print("ffmpeg -loglevel panic -i +"+inputfile+" -ss $starttime -t $endtime -f image2 -qscale 2 $filename-%d.jpeg -deinterlace -r "+ str(1/interval_secs))
+#ffmpegstring =  ("ffmpeg -v quiet -i "+inputfile+" -ss "+ str(starttime) +" -t "+str(endtime)+" -f image2 -qscale 2 " + filename+"-%d.jpeg -deinterlace -r "+ str(1/interval_secs))
+ffmpegstring =  "ffmpeg -i "+inputfile+" -y -ss "+ str(starttime) +" -t "+str(endtime)+" -an -qscale 0 -f image2 -r "+str(1/interval_secs)+ " -deinterlace " + filename+"-%d.jpeg"
+print (ffmpegstring)
+os.system(ffmpegstring)
 ## Create sequence and use the filename for the jpegs to avoid collision with other jpeg processing...
 #ffmpeg -loglevel panic -i $inputfile -ss $starttime -t $endtime -f image2  -r 1/$interval_secs -qscale 2 $filename-%d.jpeg -deinterlace 
 ##fps=1/$interval_secs 
 
 ## Get a listing of the thumbnail filenames...
+p = Path('.')
+arr=(p.glob(filename+'*.jpeg'))
+#print(list(arr))
+#res =  filter(re.math("*\d*"),list(arr))
+#res = filter(re.match(r'.*',arr))
+fns=filename+"\-\d\d\.jpeg"
+print (fns)
+fn = re.compile(fns)
+res = [f for f in list(arr) if fn.search(f.name)]
+#for f in list(arr):
+#    print(f.name+"\n")
+
+print(list(res))
 #arr=( $(ls $filename*.jpeg) )
-## Number of filenames
-#qnt=${#arr[@]}
+# Number of filenames
+qnt=len(list(res))
 ## First filename will be #1..
-#i=1
+for i in range(1,qnt):
 #while [ $i -le $qnt ]; do
-    #let totalsec=`echo "$i*$interval_secs" | bc`
-##    $echo $totalsec
-    ##i*interval_secs
-    #let timestampsecs=`echo "$totalsec+$starttime" | bc`
-    ##totalsec+starttime
-    #let sec=`echo "($timestampsecs % 60)" | bc`
-    ##(timestampsecs % 60)
-    #let min=`echo "($timestampsecs % 3600)/60" | bc`
-    ##(timestampsecs % 3600)/60
-    #let hour=`echo "($timestampsecs/3600)" | bc`
+    totalsec=i*interval_secs
+    print(totalsec)
+    timestampsecs=totalsec+starttime
+    sec=(timestampsecs % 60)
+    min=(timestampsecs % 3600)/60
+    hour=(timestampsecs/3600)
     ##timestampsecs/3600
-    #thumb_filename=$(printf "%s-%02d_%02d_%02d-%d.jpeg" "$filename" "$hour" "$min" "$sec" "$interval_secs")
-    #echo $thumb_filename
+    filedelta=timedelta(seconds=timestampsecs)
+    print(filedate+filedelta)
+    thumb_filename="%s-%02d_%02d_%02.2f-%.2f.jpeg" % (filename,hour,min,sec,interval_secs)
+    print(thumb_filename)
     #mv $filename-$i.jpeg $outdir/$thumb_filename
     #let i=i+1
 #done
